e34d402b05ecff4b12e8d6671f4f295dbf018ad4
Lisa Folliard <lisaf37@gmail.com>
2015-10-23 14:00:45 -0700

    add user controller and show view

--- jumpstart/app/controllers/images_controller.rb
+++ jumpstart/app/controllers/images_controller.rb
@@ -6,4 +6,21 @@ class ImagesController < ApplicationController
     @user = User.find(params[:user_id])
     @image = @user.images.new
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @image = @user.images.new(image_params)
+    if @image.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:image, :description)
+  end
+
 end
