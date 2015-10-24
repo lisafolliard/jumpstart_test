3bd91c3a5a827c4a135bc54947d0ca100410d62b
Lisa Folliard <lisaf37@gmail.com>
2015-10-23 18:54:15 -0700

    add css styling

--- jumpstart/db/schema.rb
+++ jumpstart/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024003731) do
+ActiveRecord::Schema.define(version: 20151023191035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 20151024003731) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
-    t.string   "about"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
